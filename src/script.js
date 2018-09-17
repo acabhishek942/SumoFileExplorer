e9f3bd705ca119b561f4868cef659840e0738ac1
Abhishek Choudhary <acabhishek942@gmail.com>
2018-09-17 06:00:34 +0530

    json contents now showing on screen

--- src/script.js
+++ src/script.js
@@ -8,30 +8,7 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 }
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-
-// httpGetAsync('long.json', function(data){console.log(data)});
-
-// Getting the webelements with data-tw-bind and setup an initial scope
-// function twoWayBinding(){
-//     console.log("I an here");
-//     var elements = document.querySelectorAll('[data-tw-bind]');
-//     console.log(elements);
-//     var scope = {};
-//     elements.forEach(function(element){
-//         console.log("Hi", element.type);
-//         if (element.type=='text' || element.type==="textarea"){
-//             var propToBind = element.getAttribute('data-tw-bind');
-//             element.onkeyup = function(){
-//                 scope[propToBind] = element.value;
-//             }
-//         }
-//     })
-
-// }
-
-// twoWayBinding();
+httpGetAsync('short.json', function(data){
+    var treeContent = document.getElementById('tree-contents');
+    treeContent.innerHTML = data;
+});

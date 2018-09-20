66b092f3552cd84853760f9d181eb18f8e392448
Abhishek Choudhary <acabhishek942@gmail.com>
2018-09-20 23:47:44 +0530

    displayed game titles in the sidebar

--- src/script.js
+++ src/script.js
@@ -8,7 +8,15 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 }
-httpGetAsync('short.json', function(data){
+httpGetAsync('long.json', function(data){
+    gameDetails = JSON.parse(data);
+    var gameTitles = [];
     var treeContent = document.getElementById('tree-contents');
-    treeContent.innerHTML = data;
+    // console.log(gameDetails);
+    for (var i = 0; i < gameDetails.length; i++){
+        gameTitles.push(gameDetails[i].title);
+        treeContent.innerHTML += '<li class="collection-item"><p> <i class="inline-icon material-icons">folder</i> <a href="..." onClick="alert("hello world"); return false;">' +  gameDetails[i].title + '</a> </p> </li>';
+    }
+    
+    // treeContent.innerHTML = data;
 });

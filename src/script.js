946bec1bc3171ccb6d8440ff60165de411a7b5dc
Abhishek Choudhary <acabhishek942@gmail.com>
2018-09-23 16:02:26 +0530

    split list rendering into element creation in js code

--- src/script.js
+++ src/script.js
@@ -9,14 +9,34 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 httpGetAsync('long.json', function(data){
+    // Make an ajax call and binf to the innerHTMl property
+    // of the list with id(tree-contents) in the fixed sidebar
     gameDetails = JSON.parse(data);
-    var gameTitles = [];
-    var treeContent = document.getElementById('tree-contents');
-    // console.log(gameDetails);
-    for (var i = 0; i < gameDetails.length; i++){
-        gameTitles.push(gameDetails[i].title);
-        treeContent.innerHTML += '<li class="collection-item"><p> <i class="inline-icon material-icons">folder</i> <a href="..." onClick="alert("hello world"); return false;">' +  gameDetails[i].title + '</a> </p> </li>';
+    var ul = document.createElement('ul');
+    ul.setAttribute('id','slide-out');
+    ul.setAttribute('class', 'sidenav show-on-med-and-down collection sidenav-fixed')
+    document.getElementById('rednderFolderList').appendChild(ul);
+    gameDetails.forEach(renderGameList);
+
+    function renderGameList(element){
+        // set all the elemets required for creating the list
+        var li = document.createElement('li');
+        var p = document.createElement('p');
+        var i = document.createElement('i');
+        var a = document.createElement('a');
+
+        // create the materialize icon of folder
+        i.setAttribute('class', 'inline-icon material-icons');
+        i.innerHTML = 'folder';
+
+        // setattribute of 'a', 'li', and add innerHTML for anchor tag
+        a.setAttribute('href', '#');
+        li.setAttribute('class', 'collection-item');
+        ul.appendChild(li);
+        li.appendChild(p);
+        p.appendChild(i);
+        p.appendChild(a);
+        a.innerHTML = element.title;
     }
     
-    // treeContent.innerHTML = data;
 });
